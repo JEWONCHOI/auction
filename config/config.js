4bc2c1c172308e40bf6e047fb45e42c2aba93de4
JWWONCHOI <alsrn6040@naver.com>
2022-04-21 14:06:16 +0900

    mysql 수정

--- config/config.js
+++ config/config.js
@@ -5,21 +5,21 @@ module.exports = {
   development: {
     username: 'root',
     password: process.env.SEQUELIZE_PASSWORD,
-    database: 'nodebird',
+    database: 'nodeauction',
     host: '127.0.0.1',
     dialect: 'mysql',
   },
   test: {
     username: "root",
     password: process.env.SEQUELIZE_PASSWORD,
-    database: "nodebird_test",
+    database: "nodeauction",
     host: "127.0.0.1",
     dialect: "mysql"
   },
   production: {
     username: 'root',
     password: process.env.SEQUELIZE_PASSWORD,
-    database: 'nodebird',
+    database: 'nodeauction',
     host: '127.0.0.1',
     dialect: 'mysql',
     logging: false,
